cac91631330b1a200173ffe759cc0b2f0c8f578d
W3TigerDog <264096224+W3TigerDog@users.noreply.github.com>
2026-09-03 09:39:23 -0600

    updated

--- python_lab_001.py
+++ python_lab_001.py
@@ -305,3 +305,21 @@ print("=" * 60)
 
 
 print("------------------------------------------------------------")
+
+print("=" * 60)
+print("010 temperature conversion program")
+print("=" * 60)
+
+unit = input("Is this temperature in Celsius or Fahrenheit (C/F): ")
+temp = float(input("Enter the temperature: "))
+
+if unit == "C":
+    temp = round(temp * 9 / 5 + 32, 1)
+    print(f"The temperature in Celsius is {temp}°F.")
+elif unit == "F":
+    temp = round((temp - 32) * 5 / 9, 1)
+    print(f"The temperature in Celsius is {temp}°C.")
+else:
+    print(f"{unit} is an invalid unit of measurement")
+
+print("------------------------------------------------------------")
