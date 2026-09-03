913bd2712ff80c5481acb71266267f9b960978b9
W3TigerDog <264096224+W3TigerDog@users.noreply.github.com>
2026-09-02 23:23:09 -0600

    updated

--- python_lab_001.py
+++ python_lab_001.py
@@ -258,3 +258,26 @@ else:
 
 print("------------------------------------------------------------")
 
+
+print("=" * 60)
+print("008 calculator program")
+print("=" * 60)
+
+operator = input("Enter an operator (+ - * /): ")
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result))
+elif operator == "*":
+    result = num1 * num2
+    print(round(result))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result))
+else:
+    print(f"{operator} is invalid.")
